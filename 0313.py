d8dfbab507492429a4bc703b33682d18d0451f31
Jones. <jonesmailhub@gmail.com>
2026-01-28 22:24:55 +0500

    Create 0313.py

---
+++ 0313.py
@@ -0,0 +1,15 @@
+dict = {
+    "dict1": {
+        "song": "Runawey",
+        "singer": "Lana Del Ray",
+        "phone": "sumsung"
+    },
+    "dict2": {
+        "name1": "Rick",
+        "name2": "Ellie",
+        "name3": "Joel"
+    }
+}
+
+print(dict["dict1"])
+print(dict["dict2"])
