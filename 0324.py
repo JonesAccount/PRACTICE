cd8c9bb58bd16e541cacbf336046d12ca258466a
Jones. <jonesmailhub@gmail.com>
2026-01-28 22:30:04 +0500

    Create 0324.py

---
+++ 0324.py
@@ -0,0 +1,10 @@
+word = "sololearn"
+print(set(word))
+
+list = ["Red", "White", "Red", "Blue", "Black", "Blue"]
+
+new_list = set(list)
+
+print(new_list)
+
+# Метод set() - создает множества по переданной последовательности удалив дубликаты.
