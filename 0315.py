6b65cf95d8d3545bc81fa7564f7d0e365668d18d
Jones. <jonesmailhub@gmail.com>
2026-01-28 22:25:57 +0500

    Create 0315.py

---
+++ 0315.py
@@ -0,0 +1,12 @@
+students = {
+    "s1": "Marry",
+    "s2": "Ellie",
+    "s3": "Joel",
+    "s4": "Cris"
+}
+
+print("Проверяем список!")
+print("Элли приша?", "s2" in students)
+print("Каел пришел?", "s5" in students)
+print("Джоэл здесь?", "s3" in students)
+print("Мэри?", "s1" in students)
