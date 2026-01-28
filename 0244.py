1ef93a925a204d6c3905edff7fd3c5a059367d10
Jones. <jonesmailhub@gmail.com>
2026-01-28 20:52:41 +0500

    Create 0244.py

---
+++ 0244.py
@@ -0,0 +1,9 @@
+name1 = "sara"
+name2 = "RUDY"
+name3 = "eLLIE"
+
+print(name1.capitalize())
+print(name2.capitalize())
+print(name3.capitalize())
+
+# Метод capitalize возвращает строку с первым символом в верхнем регистре, при этом все остальные символы будут находиться в нижнем регистре.
