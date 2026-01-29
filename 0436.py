a366629293115f2201958da3e72e8cf2d96472e9
Jones. <jonesmailhub@gmail.com>
2026-01-29 09:15:40 +0500

    Delete 0436.py

--- 0436.py
+++
@@ -1,7 +0,0 @@
-num1 = input("Первое число: ")
-num2 = input("Второе число: ")
-
-transformation_num1 = int(num1)
-transformation_num2 = int(num2)
-
-print(pow(transformation_num1, transformation_num2))
