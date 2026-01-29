8da1d12ebf02335141de065736e6bb72345feb0c
Jones. <jonesmailhub@gmail.com>
2026-01-29 10:30:19 +0500

    Create 0528.py

---
+++ 0528.py
@@ -0,0 +1,17 @@
+tree = 56
+stoun = 12
+coin = 1247
+
+list = [5 + 5, 4 - 2, 9 * 2]
+list2 = [pow(3, 2), 6]
+list3 = [0]
+list4 = [min(5, 8, 2, 8, 4), max(4, 7, 2, 6, 9)]
+list5 = [not True]
+list6 = ["Дерево: " + str(tree), "Камень: " + str(stoun), "Монеты: " + str(coin)]
+
+print(list)
+print(list2)
+print(list3)
+print(list4)
+print(list5)
+print(list6)
