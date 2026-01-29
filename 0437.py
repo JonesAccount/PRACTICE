e4dcf95071319bcdb5cb0a403afe7b373bd79b33
Jones. <jonesmailhub@gmail.com>
2026-01-29 09:32:51 +0500

    Create 0437.py

---
+++ 0437.py
@@ -0,0 +1,20 @@
+i = 0
+if not i == 1:
+    print("Hello")
+else:
+    print("Bye")
+    
+if not i == 0:
+    print("Верно")
+else:
+    print("Не верно")
+    
+if not pow(3, (5 - 3)) == 9:
+    print("Круто")
+else:
+    print("Не круто")
+
+if not abs(-1) != 1:
+    print("Мама")
+else:
+    print("Мами")
