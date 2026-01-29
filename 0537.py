197b3dbc7b4ecce39f2f75f6dcb7c252be5edf18
Jones. <jonesmailhub@gmail.com>
2026-01-29 10:33:17 +0500

    Create 0537.py

---
+++ 0537.py
@@ -0,0 +1,6 @@
+list = [6, 8, 3, 9, ["Tatata", [6.7, 34.9, 35.356, [["Finish"], True, not True], 23565.45], "Bababa", "tuktuktuk"]]
+
+print(list[-1][1][-2][0], end="\n\n")
+
+list[1] = 0
+print(list)
